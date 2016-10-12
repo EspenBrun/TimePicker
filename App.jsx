0c662f73cd2cd65a668b74a7e14421e97b7b101f
Espen K. Brun <espen_brun@hotmail.com>
2016-10-12 16:13:40 +0200

    Initial commit. Hello world is working with hot localhost. Syntax is not require

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+            Hello World!!!!! Shiiet yeah!
+         </div>
+      );
+   }
+}
+
+export default App;
