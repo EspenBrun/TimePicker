83eb4ed248ab55c1a3fee946b51a9be19fbf309c
Espen K. Brun <espen_brun@hotmail.com>
2016-10-13 23:06:54 +0200

    Just a small change, still berfore bed

--- main.js
+++ main.js
@@ -6,11 +6,9 @@ import Button from './Button.jsx';
 import TimepickerExample from './TimepickerExample.jsx';
 import TimeField from './TimeField.jsx';
 import TimePicker from './TimePicker.jsx';
-import TimePicker2 from 'rc-time-picker';
 
 ReactDOM.render(<App />, document.getElementById('app'));
 ReactDOM.render(<Button />, document.getElementById('button'));
 ReactDOM.render(<TimepickerExample />, document.getElementById('timepicker-example'));
 ReactDOM.render(<TimeField />, document.getElementById('timefield'));
 ReactDOM.render(<TimePicker />, document.getElementById('timepicker'));
-ReactDOM.render(<TimePicker2 />, document.getElementById('timepicker2'));
