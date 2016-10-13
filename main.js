10aee9672190100e881647590a7f51dd96ee0164
Espen K. Brun <espen_brun@hotmail.com>
2016-10-13 18:04:29 +0200

    Working timepicker example

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App.jsx';
 import Button from './Button.jsx';
+import TimepickerExample from './TimepickerExample.jsx';
 
 ReactDOM.render(<App />, document.getElementById('app'));
-
-ReactDOM.render(<Button/>, document.getElementById('button'));
+ReactDOM.render(<Button />, document.getElementById('button'));
+ReactDOM.render(<TimepickerExample />, document.getElementById('timepicker-example'));
