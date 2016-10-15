77efa5765f296a9900c11c7f88aaad82ce63d859
Espen K. Brun <espen_brun@hotmail.com>
2016-10-15 21:22:33 +0200

    Added some comments to Modal

--- Modal.jsx
+++ Modal.jsx
@@ -1,13 +1,14 @@
 var React = require('react');
 
-// This component creates the popup when the timeField is clicked
+// This component creates the popup Modal when the timeField is clicked
 
 var Modal = React.createClass({
 
+// the event handler for the onClick event on the backdrop
   close: function(e) {
-    e.preventDefault(); 
+    e.preventDefault(); //what does this do 
 
-    if (this.props.onClose) {
+    if (this.props.onClose) { //what does this if mean? If a method, call the method?
       this.props.onClose();
     }
   },
@@ -15,20 +16,22 @@ var Modal = React.createClass({
 // the render function
   render: function(){
 
-    // if the Modal should open, render the children. 
-    // if the Modal should not open, return null.
-    // returning null tells React that I do not want anything rendered
     if (this.props.isOpen === true){
+      // if the Modal should open, render the children
+      // and render the backdrop
+      // the backdrop is not a children, since it should have the modalStyle
       return (
         <div>
           <div id="modalStyle">
-            {this.props.children}
+            {this.props.children} 
           </div>   
           <div id="backdropStyle" onClick={e => this.close(e)}/>
         </div>
       )
     }
     else{
+      // if the Modal should not open, return null.
+      // returning null tells React that I do not want anything rendered
       return null; 
     }
   }
