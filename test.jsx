b2dfa22dbc11f8f97be4b576e9c5f4c9ad79c6d7
Espen K. Brun <espen_brun@hotmail.com>
2016-10-16 19:00:44 +0200

    componentDidUpdate was not wat I was looking for now. It was renderMinutesBubbles. Swithched out the code in Test with the bulle code

--- test.jsx
+++ test.jsx
@@ -44,6 +44,48 @@ var Test = React.createClass({
 		};
 	}, 
 
+renderMinutesBubbles: function () {
+        var minutes   = this.state.minutes;
+        var positions = this.state.positionsMinutes;
+
+        var x;
+        var y;
+
+        var onClick;
+        var onMouseMove;
+
+        var minute  = 0;
+        var bubbles = [];
+
+        for (; minute < positions.length; ++minute) {
+            x = positions[minute][0];
+            y = positions[minute][1];
+
+            onClick     = this.onClickMinute(minute);
+            onMouseMove = this.onMouseMoveMinute(minute);
+
+            bubbles.push(React.createElement(
+                'g',
+                {
+                    key: minute,
+
+                    className: 'timepicker-bubble' + (minute % 5 !== 0 ? ' small' : '') + (minutes === minute ? ' active' : ''),
+
+                    onClick:     onClick,
+                    onMouseMove: onMouseMove
+                },
+                React.createElement('circle', { cx: x, cy: y, r: minute % 5 !== 0 ? minutes === minute ? 5 : 0 : 15 }),
+                minute % 5 === 0 ? React.createElement(
+                    'text',
+                    { x: x, y: y },
+                    minute
+                ) : React.createElement('circle', { cx: x, cy: y, r: 10 })
+            ));
+        }
+
+        return bubbles;
+    },
+
   render: function () {
     return (
       <button>
