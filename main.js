f67e89e0a2a7c19e32a4615b32b8fe71c2a5f60a
Espen K. Brun <espen_brun@hotmail.com>
2016-10-20 14:20:49 +0200

    Should be compatible now! No change in code, jsut for reference

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var TimeField = require('./TimeField.jsx');
-
+ 
 ReactDOM.render(<TimeField />, document.getElementById('timefield1'));
 ReactDOM.render(<TimeField />, document.getElementById('timefield2'));
 ReactDOM.render(<TimeField />, document.getElementById('timefield3'));
