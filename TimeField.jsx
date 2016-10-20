876dc0803478d663d1462fbcd0151a9a7ef98313
Espen K. Brun <espen_brun@hotmail.com>
2016-10-20 21:16:53 +0200

    Timefield header know clickable

--- TimeField.jsx
+++ TimeField.jsx
@@ -30,6 +30,19 @@ var TimeField = React.createClass({
 			visible: true});
 	},
 
+	showHours: function(){
+		this.setState({
+			visible: true
+		});
+	},
+
+	showMinutes: function(){
+		this.setState({
+			visible: false
+		});
+	},
+
+
 	// eventHandler function that will be passed to the TimePicker, which will let that 
 	//		component update this parent components state
 	// When called, updates minutes and hours, and automatically shows minute timeface when hour is picked
@@ -44,6 +57,8 @@ var TimeField = React.createClass({
 		// Adding a zero infront of hour/minute if it only has one digit. For displaying
 		var displayHours = this.state.hours>=10 ? (this.state.hours==24 ? '00' : this.state.hours) : '0' + this.state.hours;
 		var displayMinutes = this.state.minutes>=10 ? this.state.minutes : '0' + this.state.minutes;
+		var hoursActive = this.state.visible ? "timeh1-active" : "timeh1-inactive";
+		var minutesActive = this.state.visible ? "timeh1-inactive" : "timeh1-active";
 
 		// Renders the TimeField, which is just a button. 
 		// This button opens the Modal, it's children are displayed, among them the TimePicker
@@ -55,7 +70,11 @@ var TimeField = React.createClass({
 				<Modal 
 					isOpen={this.state.isModalOpen} 
 					onClose={this.closeModal}>
-					<h1 className="timeh1">{displayHours}:{displayMinutes}</h1>  
+					<h1 className="timeh1">
+						<span className={hoursActive} onClick={this.showHours}>{displayHours}</span>
+						:
+						<span className={minutesActive} onClick={this.showMinutes}>{displayMinutes}</span>
+					</h1>  
 					<TimePicker
 					onChange={this.updateTime}
 					visible={this.state.visible}
